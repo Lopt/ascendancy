a12b35677e0ce578c05fe9cd1c9ece4ccd05d850
Chris Arnold <chris.arnold@freenet.de>
2015-04-24 10:48:10 +0200

    finish mockups, create NetworkController.cs and RegionController.cs in client.Common

--- client/client/client.Common/Try-Tests/Network.cs
+++ client/client/client.Common/Try-Tests/Network.cs
@@ -8,40 +8,38 @@ using Newtonsoft.Json;
 
 namespace client.Common
 {
-    public class Network
-    {
-        HttpClient client;
+	public class Network
+	{
+		HttpClient client;
 
-        public string antwort{ get; private set; }
+		public string antwort{ get; private set; }
 
-        public Network()
-        {
-            antwort = "";
-            client = new HttpClient(new NativeMessageHandler());
-        }
+		public Network ()
+		{
+			antwort = "";
+			client = new HttpClient (new NativeMessageHandler ());
+		}
 
-        public async Task Test()
-        {
+		public async Task Test ()
+		{
            
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(new Uri("http://derfalke.no-ip.biz/world/2594/1636/germany-41505-26190.json"));
-                if (response != null)
-                {
-                    response.EnsureSuccessStatusCode();
-                    var erg = await response.Content.ReadAsStringAsync();
-                    antwort = JsonConvert.DeserializeObject<int[,]>(erg)[0, 1].ToString();
-                }
-            }
-            catch (Exception ex)
-            {
-                antwort = ex.Message;
-                throw ex;
-            }
+			try {
+				HttpResponseMessage response = await client.GetAsync (new Uri ("http://derfalke.no-ip.biz/world/2594/1636/germany-41505-26190.json"));
+				if (response != null) {
+					response.EnsureSuccessStatusCode ();
+					var erg = await response.Content.ReadAsStringAsync ();
+					antwort = JsonConvert.DeserializeObject<int[,]> (erg) [0, 1].ToString ();
+				}
+			} catch (Exception ex) {
+				antwort = ex.Message;
+				throw ex;
+			}
 
        
-        }
-    }
+		}
+
+
+	}
 
    
 }
