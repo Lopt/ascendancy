ad8dadd3508d8b9206e82bf99a809a0f1c7b22f3
Thomas Volkmann <tombo85@web.de>
2015-04-27 18:09:33 +0200

    changes on server and interfaces

--- server/Global.asax.cs
+++ server/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿
-﻿using System;
+﻿using server.Models.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -30,6 +32,7 @@ namespace server
 
 		protected void Application_Start ()
 		{
+            Database.SetInitializer(new ASDInitializer());
 			AreaRegistration.RegisterAllAreas ();
 			RegisterGlobalFilters (GlobalFilters.Filters);
 			RegisterRoutes (RouteTable.Routes);
