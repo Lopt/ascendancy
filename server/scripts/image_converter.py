755452af5ea49c75a86db19852fae922326bd7f2
leto <leto.main@googlemail.com>
2015-04-29 13:22:23 +0200

    add ISOMETRIC REZIED Images

--- server/scripts/image_converter.py
+++ server/scripts/image_converter.py
@@ -5,16 +5,25 @@ import os.path
 
 
 PATH = "/home/leto/Projekte/MonoDevelop/ascendancy/client/data/images/"
+EMPTY_IMG = "/home/leto/Projekte/MonoDevelop/ascendancy/client/data/images/__empty.png"
 NEW_SIZE = (175, 150)
+OFFSET = (44, 0)
 
 
+empty_image = Image.open(EMPTY_IMG)
+empty_image = empty_image.resize((NEW_SIZE[0] + OFFSET[0], NEW_SIZE[1] + OFFSET[1]))
 
 for root, folders, files in os.walk(PATH):
     for file in files:
-        im = Image.open(os.path.join(root, file))
-        print os.path.join(root, file)
-        new_img = im.resize(NEW_SIZE)
-        new_img.save(os.path.join(root, "RESIZED-" + file.split('.')[0] + ".png"), "png")
+        if file.startswith("RESIZED"): continue
+        template = empty_image.copy()
+        img = Image.open(os.path.join(root, file))
+        new_img = img.resize(NEW_SIZE)
+
+        template.paste(new_img, ((0, 0, NEW_SIZE[0], NEW_SIZE[1])))
+        template = template.offset(*OFFSET)
+
+        template.save(os.path.join(root, "RESIZED-ISOMETRIC-" + file.split('.')[0] + ".png"), "png")
     break
 
 
