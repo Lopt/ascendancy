8892bcca7a7060785b0704955eacac7c526b2926
leto <leto.main@googlemail.com>
2015-06-03 09:13:28 +0200

    adds definition manager (now really)

---
+++ base/Models/Manager/DefinitionManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using @base.model.definitions;
+
+namespace @base.model
+{
+    public class DefinitionManager
+	{
+        public DefinitionManager()
+        {
+            m_definitions = new ConcurrentDictionary<int, Definition> ();
+        }
+            
+        public Definition GetDefinition(int id)
+        {
+            return m_definitions[id];
+        }
+
+        public void AddDefinition(Definition definition)
+        {
+            m_definitions[definition.ID] = definition;
+        }
+
+        private ConcurrentDictionary<int, Definition> m_definitions;
+
+	}
+}
+
