4b04d338f4c118a54e5580b3a0025fd586c8fd62
Bernd Schmidt <leto.larp@googlemail.com>
2015-07-15 19:55:35 +0200

    simplifies network controller

--- client/client/client.Common/Controllers/NetworkController.cs
+++ client/client/client.Common/Controllers/NetworkController.cs
@@ -71,8 +71,24 @@ namespace client.Common.Controllers
             }
         }
 
-        private void Request()
+        private async Task<string> RequestAsync(string url)
         {
+            string json = null;
+            try
+            {
+                HttpResponseMessage response = await m_client.GetAsync(new Uri(url));
+                if (response != null)
+                {
+                    response.EnsureSuccessStatusCode();
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                ExceptionMessage = ex.Message;
+                throw ex;
+            }
+            return json;
         }
 
         /// <summary>
@@ -81,24 +97,9 @@ namespace client.Common.Controllers
         /// <param name="jsonRegionServerPath">Json region server path.</param>
         public async Task<TerrainDefinition[,]> LoadTerrainsAsync(RegionPosition regionPosition)
         {
-            string path = Core.Helper.NetworkHelper.ReplacePath(ClientConstants.REGION_SERVER_PATH, regionPosition);
-            try
-            {
-                HttpResponseMessage response = await m_client.GetAsync(new Uri(path));
-                if (response != null)
-                {
-                    response.EnsureSuccessStatusCode();
-                    var json = await response.Content.ReadAsStringAsync();
-                    return JsonConvert.DeserializeObject<TerrainDefinition[,]>(json);
-                }
-            }
-            catch (Exception ex)
-            {
-                ExceptionMessage = ex.Message;
-                throw ex;
-            }
-            return null;
-
+            var path = Core.Helper.NetworkHelper.ReplacePath(ClientConstants.REGION_SERVER_PATH, regionPosition);
+            var json = await RequestAsync(path);
+            return JsonConvert.DeserializeObject<TerrainDefinition[,]>(json);
         }
 
         /// <summary>
@@ -106,22 +107,9 @@ namespace client.Common.Controllers
         /// </summary>
         public async Task<Core.Models.Definitions.TerrainDefinition[]> LoadTerrainTypesAsync()
         {
-            try
-            {
-                HttpResponseMessage response = await m_client.GetAsync(new Uri(ClientConstants.TERRAIN_TYPES_SERVER_PATH));
-                if (response != null)
-                {
-                    response.EnsureSuccessStatusCode();
-                    var json = await response.Content.ReadAsStringAsync();
-                    return JsonConvert.DeserializeObject<Core.Models.Definitions.TerrainDefinition[]>(json);
-                }
-            }
-            catch (Exception ex)
-            {
-                ExceptionMessage = ex.Message;
-                throw ex;
-            }
-            return null;
+            string path = ClientConstants.TERRAIN_TYPES_SERVER_PATH;
+            var json = await RequestAsync(path);
+            return JsonConvert.DeserializeObject<Core.Models.Definitions.TerrainDefinition[]>(json);
         }
 
         /// <summary>
@@ -129,22 +117,9 @@ namespace client.Common.Controllers
         /// </summary>
         public async Task<Core.Models.Definitions.UnitDefinition[]> LoadUnitTypesAsync()
         {
-            try
-            {
-                HttpResponseMessage response = await m_client.GetAsync(new Uri(ClientConstants.UNIT_TYPES_SERVER_PATH));
-                if (response != null)
-                {
-                    response.EnsureSuccessStatusCode();
-                    var json = await response.Content.ReadAsStringAsync();
-                    return JsonConvert.DeserializeObject<Core.Models.Definitions.UnitDefinition[]>(json);
-                }
-            }
-            catch (Exception ex)
-            {
-                ExceptionMessage = ex.Message;
-                throw ex;
-            }
-            return null;
+            string path = ClientConstants.UNIT_TYPES_SERVER_PATH;
+            var json = await RequestAsync(path);
+            return JsonConvert.DeserializeObject<Core.Models.Definitions.UnitDefinition[]>(json);
         }
 
 
@@ -157,40 +132,28 @@ namespace client.Common.Controllers
         /// <param name="password">Password.</param>
         public async Task<Account> LoginAsync(Core.Models.Position currentGamePosition, string user, string password)
         {
-            try
+            Account account = null;
+            var request = new Core.Connections.LoginRequest(currentGamePosition, user, password);
+            var json = JsonConvert.SerializeObject(request);
+
+            var path = ClientConstants.LOGIN_PATH;
+            path = path.Replace(ClientConstants.LOGIC_SERVER_JSON, json);
+
+            var newPath = ClientConstants.LOGIC_SERVER + path;
+            var jsonFromServer = await RequestAsync(newPath);
+
+            var loginResponse = JsonConvert.DeserializeObject<Core.Connections.LoginResponse>(jsonFromServer);
+            if (loginResponse.Status == Core.Connections.LoginResponse.ReponseStatus.OK)
             {
-                Account account = null;
-                var request = new Core.Connections.LoginRequest(currentGamePosition, user, password);
-                var json = JsonConvert.SerializeObject(request);
-
-                var path = ClientConstants.LOGIN_PATH;
-                path = path.Replace(ClientConstants.LOGIC_SERVER_JSON, json);
-
-                HttpResponseMessage response = await m_client.GetAsync(new Uri(ClientConstants.LOGIC_SERVER + path));
-                if (response != null)
-                {
-                    response.EnsureSuccessStatusCode();
-                    var jsonFromServer = await response.Content.ReadAsStringAsync();
-
-                    var loginResponse = JsonConvert.DeserializeObject<Core.Connections.LoginResponse>(jsonFromServer);
-                    if (loginResponse.Status == Core.Connections.LoginResponse.ReponseStatus.OK)
-                    {
-                        m_sessionID = loginResponse.SessionID;
-                        account = new Account(loginResponse.AccountId, user);
-                    }
-                    else
-                    {
-                        ExceptionMessage = "Login failure";
-                    }
-
-                }
-                return account;
+                m_sessionID = loginResponse.SessionID;
+                account = new Account(loginResponse.AccountId, user);
             }
-            catch (Exception ex)
+            else
             {
-                ExceptionMessage = ex.Message;
-                throw ex;
+                ExceptionMessage = "Login failure";
             }
+
+            return account;
         }
 
         /// <summary>
@@ -200,71 +163,41 @@ namespace client.Common.Controllers
         /// <param name="currentGamePosition">Current game position.</param>
         /// <param name="regionPositions">Region positions.</param>
         public async Task<Core.Connections.Response> LoadEntitiesAsync(Core.Models.Position currentGamePosition, RegionPosition[] regionPositions)
-        {
-            try
+        {            
+            var request = new Core.Connections.LoadRegionsRequest(m_sessionID, currentGamePosition, regionPositions);
+            var json = JsonConvert.SerializeObject(request);
+
+            var path = ClientConstants.LOAD_REGIONS_PATH;
+            path = path.Replace(ClientConstants.LOGIC_SERVER_JSON, json);
+            path = ClientConstants.LOGIC_SERVER + path;
+
+            var jsonFromServer = await RequestAsync(path);
+            var entitiesResponse = JsonConvert.DeserializeObject<Core.Connections.Response>(jsonFromServer);
+
+            if (entitiesResponse.Status == Core.Connections.Response.ReponseStatus.OK)
             {
-                var request = new Core.Connections.LoadRegionsRequest(m_sessionID, currentGamePosition, regionPositions);
-                var json = JsonConvert.SerializeObject(request);
-
-                var path = ClientConstants.LOAD_REGIONS_PATH;
-                path = path.Replace(ClientConstants.LOGIC_SERVER_JSON, json);
-
-                HttpResponseMessage response = await m_client.GetAsync(new Uri(ClientConstants.LOGIC_SERVER + path));
-                if (response != null)
-                {
-                    response.EnsureSuccessStatusCode();
-                    var jsonFromServer = await response.Content.ReadAsStringAsync();
-
-                    var entitiesResponse = JsonConvert.DeserializeObject<Core.Connections.Response>(jsonFromServer);
-
-                    if (entitiesResponse.Status == Core.Connections.Response.ReponseStatus.OK)
-                    {
-                        return entitiesResponse;
-                    }
-
-                }
-                return new Core.Connections.Response();
-
+                return entitiesResponse;
             }
-            catch (Exception ex)
-            {
-                ExceptionMessage = ex.Message;
-                throw ex;
-            }
+            return new Core.Connections.Response();
         }
 
         public async Task<bool> DoActionsAsync(Core.Models.Position currentGamePosition, Core.Models.Action[] actions)
         {
-            try
+            var request = new Core.Connections.DoActionsRequest(m_sessionID, currentGamePosition, actions);
+            var json = JsonConvert.SerializeObject(request);
+
+            var path = ClientConstants.DO_ACTIONS_PATH;
+            path = path.Replace(ClientConstants.LOGIC_SERVER_JSON, json);
+            path = ClientConstants.LOGIC_SERVER + path;  
+            var jsonFromServer = await RequestAsync(path);
+
+
+            var entitiesResponse = JsonConvert.DeserializeObject<Core.Connections.Response>(jsonFromServer);
+            if (entitiesResponse.Status == Core.Connections.Response.ReponseStatus.OK)
             {
-                var request = new Core.Connections.DoActionsRequest(m_sessionID, currentGamePosition, actions);
-                var json = JsonConvert.SerializeObject(request);
-
-                var path = ClientConstants.DO_ACTIONS_PATH;
-                path = path.Replace(ClientConstants.LOGIC_SERVER_JSON, json);
-
-                HttpResponseMessage response = await m_client.GetAsync(new Uri(ClientConstants.LOGIC_SERVER + path));
-                if (response != null)
-                {
-                    response.EnsureSuccessStatusCode();
-
-                    var jsonFromServer = await response.Content.ReadAsStringAsync();
-
-                    var entitiesResponse = JsonConvert.DeserializeObject<Core.Connections.Response>(jsonFromServer);
-                    if (entitiesResponse.Status == Core.Connections.Response.ReponseStatus.OK)
-                    {
-                        return true;
-                    }
-
-                }
-                return false;
-
+                return true;
             }
-            catch (Exception ex)
-            {
-                ExceptionMessage = ex.Message;
-                throw ex;
-            }
+            return false;
         }
 
 
