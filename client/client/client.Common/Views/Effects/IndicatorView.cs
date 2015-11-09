80a69eb63815fd34ee50579a21864c5782d58543
collamar <gantz@gmx.net>
2015-11-09 18:57:44 +0100

    added effectlayer, effectnode, some healthbar sprites

---
+++ client/client/client.Common/Views/Effects/IndicatorView.cs
@@ -0,0 +1,27 @@
+﻿namespace Client.Common.Views.Effects
+{
+    using CocosSharp;
+    /// <summary>
+    /// Indicator view.
+    /// </summary>
+    public class IndicatorView
+    {
+        public IndicatorView(CCTileMapCoordinates center, CCTileMapLayer layer)
+        {
+            m_center = center;
+            m_layer = layer;
+
+        }
+
+        /// <summary>
+        /// The center point for the indicator
+        /// </summary>
+        private CCTileMapCoordinates m_center;
+
+        /// <summary>
+        /// The layer which should be colored as indicator.
+        /// </summary>
+        private CCTileMapLayer m_layer;
+
+    }
+}
